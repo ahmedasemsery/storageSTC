5e684eba4326a5115fcdc58abae047b38c8d7319
Asem <ahmedasemsery@gmail.com>
2023-07-29 16:28:20 +0300

    Initialize

---
+++ src/main/java/stc/assessments/storagestc/StorageStcApplication.java
@@ -0,0 +1,13 @@
+package stc.assessments.storagestc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageStcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageStcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/stc/assessments/storagestc/StorageStcApplicationTests.java
@@ -0,0 +1,13 @@
+package stc.assessments.storagestc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageStcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
